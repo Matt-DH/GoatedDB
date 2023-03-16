feaaac34097845a81dc7fb481edccb931b4e96bd
mattdh <mattdavidhardy@gmail.com>
2023-03-16 19:08:08 -0230

    Changes by calem

--- src/QuoteLibrary.java
+++ src/QuoteLibrary.java
@@ -1,2 +1,31 @@
-public class QuoteLibrary {
+import java.util.LinkedList;
+import java.util.List;
+
+public class QuoteLibrary{
+    private List<QuoteAuto> AutoList;
+    private List<QuoteHome> HomeList;
+
+    public QuoteLibrary(){
+        this.AutoList = new LinkedList<>();
+        this.HomeList = new LinkedList<>();
+    }
+
+    public QuoteLibrary(LinkedList<QuoteAuto> autoList, LinkedList<QuoteHome> homeList) {
+        this.AutoList = autoList;
+        this.HomeList = homeList;
+    }
+
+    public List<QuoteAuto> getAutoList(){
+        return AutoList;
+    }
+    public void setAutoList(List<QuoteAuto> AutoList){
+        this.AutoList = AutoList;
+    }
+    public List<QuoteHome> getHomeList(){
+        return HomeList;
+    }
+    public void setHomeList(List<QuoteHome> HomeList){
+        this.HomeList = HomeList;
+    }
+
 }
